11dd25f260d8ac08670c4959a5cacf7275a1ad83
isaac-pj <isaac-pj@hotmail.com>
2020-12-16 19:25:03 -0300

    feat(deploy): text revision and sdk version

--- src/app/pages/contact/contact.page.ts
+++ src/app/pages/contact/contact.page.ts
@@ -1,50 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { NavController, Platform } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { NavController, Platform } from "@ionic/angular";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.page.html',
-  styleUrls: ['./contact.page.scss'],
+  selector: "app-contact",
+  templateUrl: "./contact.page.html",
+  styleUrls: ["./contact.page.scss"],
 })
 export class ContactPage implements OnInit {
-
   expanded = false;
-  orientation = '';
-  phone: 'tel:88997138594';
-  facebook = 'https://www.facebook.com/isaac.lima.7549';
-  mail = 'mailto:isaac-pj@hotmail.com';
+  orientation = "";
+  phone: "tel:88997138594";
+  facebook = "https://www.facebook.com/isaac.lima.7549";
+  mail = "mailto:isaac-pj@hotmail.com";
 
-  short = `O Judah's Lion Studio, é uma empresa fundada e administrada pelo designer digital 
-  Isaac Brandão Lima, que atua nas áreas de design e desenvolvimento de softawre,
-  desenvolvendo...`;
+  short = `A Crown Studio, é uma pequena empresa fundada e administrada pelo designer digital 
+  Isaac Brandão Lima, que atua nas áreas de design e desenvolvimento de software, oferecendo 
+  soluções...`;
 
-  text = `O Judah's Lion Studio, é uma empresa fundada e administrada pelo designer digital 
-  Isaac Brandão Lima, que atua nas áreas de design e desenvolvimento de softawre,
-  desenvolvendo soluções de design digitais e impressas, para seus clientes. Entre 
-  os serviços disponíveis estão: a criação de artes gráficas em geral como, marca, 
-  idetidade visual, cartazes, cartão de visita, etc.. Edição de Imagens, Audio e Video.
-  Illustração manual, vetorial e pintura digital e animação 2D. Modelagem e animação 3D.
-  UX design, criação de web sites, aplicativos mobile e desktop, desenvolvimento de 
-  jogos 2D e 3D, entre outros.`;
+  text = `A Crown Studio, é uma pequena empresa fundada e administrada pelo designer digital 
+  Isaac Brandão Lima, que atua nas áreas de design e desenvolvimento de software, oferecendo 
+  soluções de design digitais e impressas, para seus clientes. Entre os serviços disponíveis 
+  estão: a criação de artes gráficas em geral como, marca, identidade visual, cartazes, cartão 
+  de visita, etc. Edição digital de imagens, áudio e vídeo. Ilustração manual, vetorial, pintura digital, 
+  animação 2D, modelagem e animação 3D. Soluções de UX design, criação de web sites, aplicativos 
+  mobile e desktop, desenvolvimento de jogos 2D e 3D, entre outros.`;
 
-  constructor(
-    public platform: Platform,
-    public navCtrl: NavController,
-  ) {
+  constructor(public platform: Platform, public navCtrl: NavController) {
     platform.ready().then((readySource) => {
-      this.orientation = platform.width() > platform.height() ? 'horizontal' : 'vertical';
+      this.orientation =
+        platform.width() > platform.height() ? "horizontal" : "vertical";
     });
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   load(url) {
-    window.open(url, '_blank');
+    window.open(url, "_blank");
   }
 
   expand() {
     this.expanded = this.expanded ? false : true;
   }
-
 }
